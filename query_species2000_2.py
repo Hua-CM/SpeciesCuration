5939956f22b97a5937a1a083382be755725ec813
Zhongyi Hua <njbxhzy@hotmail.com>
2025-05-24 10:39:05 +0800

    add authorship in output

--- query_species2000_2.py
+++ query_species2000_2.py
@@ -9,7 +9,13 @@
 import pandas as pd
 
 
-def query_species(query_name):
+def query_species(species_df, query_name):
+    """
+    Query species from the species dataframe
+    :param species_df: DataFrame, species dataframe
+    :param query_name: str, query species name
+    :return: str, curated species name
+    """
 
     def get_sci(_dict, _idx):
         if _dict['taxonRank'][_idx] == 'species':
@@ -30,7 +36,7 @@ def query_species(query_name):
     name_list = query_name.split(' ')
     try:
         if len(name_list) == 2:
-            query_result = species2019[species2019['scientificName'].str.match(query_name)]
+            query_result = species_df[species_df['scientificName'].str.match(query_name)]
             query_dict = query_result[query_result['verbatimTaxonRank'].isna()].to_dict('list')
             if query_result.empty:
                 return 'Not in species2000'
@@ -39,11 +45,11 @@ def query_species(query_name):
                 sci_name = get_sci(query_dict, tmp_idx)
                 return sci_name
             else:
-                query_dict = species2019[species2019['taxonID'] == query_dict['acceptedNameUsageID'][0]].to_dict('list')
+                query_dict = species_df[species_df['taxonID'] == query_dict['acceptedNameUsageID'][0]].to_dict('list')
                 sci_name = get_sci(query_dict, 0)
                 return sci_name
         elif len(name_list) == 4:
-            query_result = species2019[species2019['scientificName'].str.match(' '.join(name_list[0:2]))]
+            query_result = species_df[species_df['scientificName'].str.match(' '.join(name_list[0:2]))]
             query_dict = query_result[query_result['infraspecificEpithet'] == name_list[3]].to_dict('list')
             if query_result.empty:
                 return 'Not in species2000'
@@ -52,7 +58,7 @@ def query_species(query_name):
                 sci_name = get_sci(query_dict, tmp_idx)
                 return sci_name
             else:
-                query_dict = species2019[species2019['taxonID'] == query_dict['acceptedNameUsageID'][0]].to_dict('list')
+                query_dict = species_df[species_df['taxonID'] == query_dict['acceptedNameUsageID'][0]].to_dict('list')
                 sci_name = get_sci(query_dict, 0)
                 return sci_name
         else:
@@ -61,21 +67,25 @@ def query_species(query_name):
         return 'Check name'
 
 
-def query_main(input_path, output_path):
-    tb1 = pd.read_table(input_path, names=['raw_name'])
-    tb1['fix_name'] = tb1['raw_name'].apply(query_species)
-    tb1.to_csv(output_path, sep='\t', index=False)
+def query_main(input_path, db_path, output_path):
+    species_df = pd.read_table(db_path)
+    result_lst = []
+    with open(input_path, encoding='utf-8') as f_in:
+        for _raw_name in f_in.read().splitlines():
+            result_lst.append({'raw_name': _raw_name,
+                               'fix_name': query_species(species_df, _raw_name)})
+    result_df = pd.DataFrame(result_lst)
+    result_df.to_csv(output_path, sep='\t', index=False)
 
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description="This is the script for fix scientific name based on species2000")
     parser.add_argument('-i', '--input', required=True,
-                        help='<file_path> imput path')
-    parser.add_argument('-d', '--database', required=True,
+                        help='<file_path> input path')
+    parser.add_argument('-d', '--db', required=True,
                         help='<file_path> Species2000 taxa file')
     parser.add_argument('-o', '--output', required=True,
                         help='<file_path> result path')
     args = parser.parse_args()
-    species2019 = pd.read_table(args.database)
-    query_main(args.input, args.output)
+    query_main(args.input, args.db, args.output)
--- query_species2000_colDB.py
+++ query_species2000_colDB.py
@@ -5,22 +5,23 @@
 # @Usage: 
 # @Note:
 # @E-mail: njbxhzy@hotmail.com
+import argparse
+from collections import defaultdict
 
 import pandas as pd
-from collections import defaultdict
 
 
 class ColDBQuery:
-    def __init__(self, DB_path, query_path):
-        self.tb_taxon = pd.read_table(DB_path)
+    def __init__(self, db_path, query_path):
+        self.tb_taxon = pd.read_table(db_path)
         self.querylst = ColDBQuery._read_(query_path)
         self.species_dict, self.reverse_dict = self.generate_dicts()
 
     @staticmethod
     def _read_(query_path):
-        with open(query_path) as f_in:
-            a = f_in.read().splitlines()
-        return a
+        with open(query_path, encoding='utf-8') as f_in:
+            contents = f_in.read().splitlines()
+        return contents
 
     def generate_dicts(self):
         species_dict = defaultdict()
@@ -28,7 +29,7 @@ class ColDBQuery:
         for _item in self.tb_taxon.itertuples():
             if _item.rank in ('species', 'subspecies', 'variety'):
                 species_dict.setdefault(_item.scientificName,
-                                        {'ID': _item.ID, 'status': _item.status, 'parentID': _item.parentID})
+                                        {'ID': _item.ID, 'status': _item.status, 'parentID': _item.parentID, 'authorship': _item.authorship})
                 """ # for subspecies
                 _tmp_list = _item.scientificName.split()
                 _tmp_name = _tmp_list[0] + ' ' + _tmp_list[1] + ' subsp. ' + _tmp_list[2] if len(_tmp_list) != 4 else ' '.join(_tmp_list)
@@ -38,39 +39,70 @@ class ColDBQuery:
         reverse_dict = {_item['ID']: _species for _species, _item in species_dict.items()}
         return species_dict, reverse_dict
 
-    def query_species(self, query_name):
+    def query_one_species(self, query_name):
+        """Query accepted name for one species in species2000 database.
+
+        Args:
+            query_name (str): _description_
+
+        Returns:
+            str: _description_
+        """
         query_res = self.species_dict.get(query_name, 'Not in species2000')
         if query_res == 'Not in species2000':
-            return query_res
+            return 'Not in Species2000, most likely a spelling error.'
         else:
             if query_res.get('status') in ('provisionally accepted', 'accepted', 'ambiguous synonym'):
                 return query_name
             # 'ambiguous synonym' just keep it
             elif query_res.get('status') == 'synonym':
-                return self.reverse_dict.get(query_res.get('parentID'))
+                if accepted_name:= self.reverse_dict.get(query_res.get('parentID')):
+                    return accepted_name
+                else:
+                    return 'There are homonymous species named by different authors.'
             else:
-                return "Please check manually"
+                return 'Please check manually'
+    
+    def query_authorship(self, query_name):
+        """Query authorship for one species in species2000 database."""
+        try:
+            query_res = self.species_dict.get(query_name).get('authorship')
+        except:
+            query_res = "Please check manually"
+        return query_res
 
-    def query(self):
+
+    def query_all(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
         res_lst = []
         for _species in self.querylst:
-            res_lst.append({'raw': _species,
-                            'curated': self.query_species(_species)})
+            tmp_dct = {'raw': _species,
+                       'curated': self.query_one_species(_species)}
+            if tmp_dct.get('curated') not in ('Not in Species2000, most likely a spelling error.',
+                                              'Please check manually',
+                                              'There are homonymous species named by different authors.'):
+                tmp_dct['authorship'] = self.query_authorship(tmp_dct.get('curated'))
+            res_lst.append(tmp_dct)
         return pd.DataFrame(res_lst)
 
 
 def main():
-    import argparse
+    
     parser = argparse.ArgumentParser(description="This is the script for fix scientific name based on species2000")
     parser.add_argument('-i', '--input', required=True,
-                        help='<file_path> imput path')
+                        help='<file_path> input NameUsage.tsv file path')
     parser.add_argument('-d', '--database', required=True,
                         help='<file_path> Species2000 taxa file')
     parser.add_argument('-o', '--output', required=True,
-                        help='<file_path> result path')
+                        help='<file_path> result path') 
+
     args = parser.parse_args()
     queryIns = ColDBQuery(args.database, args.input)
-    tb1 = queryIns.query()
+    tb1 = queryIns.query_all()
     tb1.to_csv(args.output, sep='\t', index=False)
 
 
